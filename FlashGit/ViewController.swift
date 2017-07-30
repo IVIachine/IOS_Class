d3b980a1970207aec81c261590d37bdfc9bfab0f
Tyler Chermely <tyler846050@aim.com>
2017-07-29 20:24:48 -0400

    Initial Commit

---
+++ FlashGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlashGit
+//
+//  Created by Tyler Chermely on 7/29/17.
+//  Copyright © 2017 Sami Vashaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
